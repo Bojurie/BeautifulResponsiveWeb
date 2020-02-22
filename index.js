9999e6a23c36ed1243a96baa7f33f4c7f0c7ea45
Bojurie Rogers-Wright <40696649+Bojurie@users.noreply.github.com>
2020-02-22 07:19:20 -0800

    added javascript and media queries

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ document.getElementById("myButton").onclick = function () {
 document.getElementById("learnMore").onclick = function () {
   location.href = "service.html";
 }
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
