b6efc4021bc3905f62c895391ad9493686988200
Bojurie Rogers-Wright <40696649+Bojurie@users.noreply.github.com>
2020-02-24 17:37:26 -0800

    javascript form submit code

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
-
 document.getElementById("learnMore").onclick = function () {
   location.href = "service.html";
 }
 
+const init = function(){
+  document.getElementById('button-send').addEventListener('click', send);
+}
+
+const send = function(ev){
+    ev.preventDefault();
+    ev.stopPropagation();
+
+    let ret = validate();
+
+    if(ret){
+          document.getElementById('form-user').submit();
+        }else{
+          let err = document.querySelector('error');
+          let input = err.querySelector('input');
+          err.setAttribute('data-errormsg', `...Missing ${input.placeholder}`);
+    }
+}
+
+const validate = function(){
+    let valid = false;
+    let fullName = document.getElementById('fullName');
+    let email = document.getElementById('email');
+    if(fullName.value ===""){
+      failures.push({input:'fullName', msg:'Required Field'})
+    }
+    if(email.value ===""){
+      failures.push({input:'email', msg:'Required Field'})
+    }
+
+    return valid;
+}
+
+
+document.addEventListener('DOMcontentLoaded', init);
