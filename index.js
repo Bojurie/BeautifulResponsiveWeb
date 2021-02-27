edfe668007a4cec3f4d05aa67825ce5daa82246b
MacBookAir <macbookair@MacBookAirs-MacBook-Air.local>
2021-02-27 03:58:47 -0800

    completeted website

--- index.js
+++ index.js
@@ -16,34 +16,34 @@ const init = function(){
 }
 
 document.getElementById("form").submit();
-// const send = function(ev){
-//     ev.preventDefault();
-//     ev.stopPropagation();
+const send = function(ev){
+    ev.preventDefault();
+    ev.stopPropagation();
 
-//     let ret = validate();
+    let ret = validate();
 
-//     if(ret){
-//           document.getElementById("form").submit();
-//         }else{
-//           let err = document.querySelector('error');
-//           let input = err.querySelector('input');
-//           err.setAttribute('data-errormsg', `...Missing ${input.placeholder}`);
-//     }
-// }
+    if(ret){
+          document.getElementById("form").submit();
+        }else{
+          let err = document.querySelector('error');
+          let input = err.querySelector('input');
+          err.setAttribute('data-errormsg', `...Missing ${input.placeholder}`);
+    }
+}
 
-// const validate = function(){
-//     let valid = false;
-//     let fullName = document.getElementById('fullName');
-//     let email = document.getElementById('email');
-//     if(fullName.value ===""){
-//       failures.push({input:'fullName', msg:'Required Field'})
-//     }
-//     if(email.value ===""){
-//       failures.push({input:'email', msg:'Required Field'})
-//     }
+const validate = function(){
+    let valid = false;
+    let fullName = document.getElementById('fullName');
+    let email = document.getElementById('email');
+    if(fullName.value ===""){
+      failures.push({input:'fullName', msg:'Required Field'})
+    }
+    if(email.value ===""){
+      failures.push({input:'email', msg:'Required Field'})
+    }
 
-//     return valid;
-// }
+    return valid;
+}
 
 
 document.addEventListener('DOMcontentLoaded', init);
